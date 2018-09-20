b037f525882e3e0e49c152ff93210bb36b3b87b6
Ivan Neto <ivan.cr.neto@gmail.com>
2018-09-20 19:51:27 -0300

    changed UploadView to use a form class

--- youtube_django/views.py
+++ youtube_django/views.py
@@ -1,6 +1,14 @@
+from django import forms
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
 
-class VideoUploadView(TemplateView):
+
+class YouTubeForm(forms.Form):
+    pass
+
+
+class VideoUploadView(FormView):
     template_name = 'youtube_django/upload.html'
+    form_class = YouTubeForm
