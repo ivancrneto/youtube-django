62f094f73bea235deece2274abe4e2c9f86c065d
Ivan Neto <ivan.cr.neto@gmail.com>
2018-09-21 01:53:31 -0300

    created view to do the first step oauth authorization with Google API

--- youtube_django/urls.py
+++ youtube_django/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from .views import VideoUploadView
+from .views import AuthorizeView, VideoUploadView
 
 urlpatterns = [
-    path('upload', VideoUploadView.as_view(), name='video_upload')
+    path('upload/', VideoUploadView.as_view(), name='video_upload'),
+    path('authorize/', AuthorizeView.as_view(), name='authorize'),
 ]
--- youtube_django/views.py
+++ youtube_django/views.py
@@ -1,7 +1,14 @@
 from django import forms
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.views.generic.base import View
 from django.views.generic.edit import FormView
+
+from oauth2client.client import flow_from_clientsecrets, OAuth2WebServerFlow
+from oauth2client.contrib import xsrfutil
+from oauth2client.contrib.django_util.storage import DjangoORMStorage
+from .models import GoogleAPIOauthInfo
 
 
 
@@ -12,3 +19,29 @@ class YouTubeForm(forms.Form):
 class VideoUploadView(FormView):
     template_name = 'youtube_django/upload.html'
     form_class = YouTubeForm
+
+
+class AuthorizeView(View):
+
+    def get(self, request, *args, **kwargs):
+        storage = DjangoORMStorage(
+            GoogleAPIOauthInfo, 'id', request.user.id, 'credential')
+        credential = storage.get()
+        flow = OAuth2WebServerFlow(
+            client_id=settings.GOOGLE_OAUTH2_CLIENT_ID,
+            client_secret=settings.GOOGLE_OAUTH2_CLIENT_SECRET,
+            scope='https://www.googleapis.com/auth/youtube',
+            redirect_uri='http://localhost:8888/oauth2callback/')
+
+        # TODO: for a downloaded the client_secrets file
+        '''flow = flow_from_clientsecrets(
+            settings.GOOGLE_OAUTH2_CLIENT_SECRETS_JSON,
+            scope='https://www.googleapis.com/auth/youtube',
+            redirect_uri='http://localhost:8888/oauth2callback/')'''
+
+        if credential is None or credential.invalid == True:
+            flow.params['state'] = xsrfutil.generate_token(
+                settings.SECRET_KEY, request.user)
+            authorize_url = flow.step1_get_authorize_url()
+            return redirect(authorize_url)
+        return redirect('/')
